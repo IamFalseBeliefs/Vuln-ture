5952c79f8bae02c47c84deb46aac44ff3e8ee7ae
Charles Steven Whitcomb JR <64711936+IamFalseBielfs@users.noreply.github.com>
2021-01-01 16:30:53 -0500

    Add files via upload

---
+++ VulScan.py
@@ -0,0 +1,11 @@
+import PortScanner
+
+
+print(" __      __    _                  _                   ")
+print(" \ \    / /   | |                | |                  ")
+print("  \ \  / /   _| |_ __    ______  | |_ _   _ _ __ ___  ")
+print("   \ \/ / | | | | '_ \  |______| | __| | | | '__/ _ \ ")
+print("    \  /| |_| | | | | |          | |_| |_| | | |  __/ ")
+print("     \/  \__,_|_|_| |_|           \__|\__,_|_|  \___| ")
+                                                     
+targets_ip = input("[----] Enter target to scan for vulnerable ports: ")
